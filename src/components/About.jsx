7c018cc5f01a8b026f0c0223a49777ab3d0a1288
abhi-python <abhilash.saini1@gmail.com>
2023-04-22 20:00:16 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name = "about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex justify-center flex-col w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>
+            As a highly accomplished and user-focused Front-end Developer, I specialize in collaborating with UI/UX and am comfortable working with JavaScript, React, HTML/CSS to execute functional specifications for websites and applications. My educational background includes a Masters of Computer Applications from the International School of Informatics and Management, a Bachelors of Computer Application from Commerce College Jaipur, and 12th grade from Tagore Public School Jaipur. 
+            </p>
+            <br/>
+            <p className='text-xl'>
+            I possess excellent skills in HTML, JavaScript, CSS, and React fundamentals, including React Components, React Principals, React lifecycle, JSX, Virtual DOM, Redux, Git, RESTful APIs, Vesrion Control, Git, IDEs, ECMAScript, Hooks, and Routers. I have professional experience as an Integration Expert at NlineAxis IT Solutions Private Limited, where I have worked on video encoding, DRM configuration, content management platforms, and streaming protocols. Additionally, I have trained in React JS at Fullestop and worked on several projects that demonstrate my proficiency in the subject. In my free time, I enjoy playing cricket, exploring new places, and listening to music.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
